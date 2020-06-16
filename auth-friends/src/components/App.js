6086ea16fb4ef9acbd16d1afb34a6dedb821f07b
meep-morp <gracieaerogirl@gmail.com>
2020-06-16 10:31:47 -0500

    Setup Axios with Auth and added more components

--- auth-friends/src/components/App.js
+++ auth-friends/src/components/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import "../styles/App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PrivateRoute from "./utils/privateRoute";
+import Nav from './Nav';
+import Login from './Login';
+import Signup from "./SignUp";
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-
-      </Router>
+      <Nav />
+      <div className="content">
+        <Router>
+          <Route path="/" exact>
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Router>
+      </div>
     </div>
   );
 }
