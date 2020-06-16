b5eda3d6c20707c8e3abe85a95540b2d8775bdff
meep-morp <gracieaerogirl@gmail.com>
2020-06-16 10:05:11 -0500

    Added router and created private route Util

---
+++ auth-friends/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../styles/App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
